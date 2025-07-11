32eb05ebc1d4abde00999a49761d47d2479d3fe7
akotetsu <yuanliqu@gmail.com>
2025-07-11 16:32:13 +0900

    Initial Commit

---
+++ GitHubPractice/GitHubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPracticeApp.swift
+//  GitHubPractice
+//
+//  Created by 原里駆 on 2025/07/11.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
